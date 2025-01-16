f5aa5985c7fe613d5497324468615e91fec132d2
jtumbokon1 <jirchgot@gmail.com>
2025-01-16 15:21:39 -0700

    Add files via upload

---
+++ Lab0/Program.cs
@@ -0,0 +1,70 @@
+﻿/*
+ * CPRG 211 Lab 0 Activity
+ * Author: Jirch Tumbokon
+ * When: Winter 2025
+ */
+
+// get low and high int numbers
+// low must be positive
+// high must be greater than low
+
+using System.Runtime.CompilerServices;
+using Lab0;
+
+int low, high;
+int diff;   // difference between low and high
+
+low = Utilities.GetPositiveInt("low number");
+
+high = Utilities.GetIntInRange("high number", low + 1, Int32.MaxValue); // the largest int possible
+
+// calculate and print the difference
+diff = high - low;
+Console.WriteLine($"The difference between {low} and {high} is {diff}");
+
+// list to store numbers
+List<int> numbers = new List<int>();
+for (int i = low; i <= high; i++)
+{
+    numbers.Add(i);
+}
+
+// display numbers in list
+Console.WriteLine("Numbers in the list:");
+for (int i = 0; i <= diff; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
+
+// create a file named "numbers.txt" and write to it the numbers from the array in reverse order
+StreamWriter streamWriter = File.CreateText("numbers.txt"); // located in the same folder as the .exe file
+for (int i = numbers.Count - 1; i >= 0; i--)
+{
+    streamWriter.WriteLine(numbers[i]);
+}
+streamWriter.Close(); // important to finalize the writing
+Console.WriteLine("File written");
+
+int sum = 0;
+
+// reads file named "numbers.txt" and gives sum of all numbers
+StreamReader streamReader = new StreamReader("numbers.txt");
+string? line;
+while ((line = streamReader.ReadLine()) != null)
+{
+    sum += int.Parse(line);
+}
+streamReader.Close();
+
+Console.WriteLine("Sum of numbers:");
+Console.WriteLine($"{sum}");
+
+// check if num is prime and prints result
+Console.WriteLine("Prime numbers:");
+for (int i = 0; i <= diff; i++)
+{
+    if (Utilities.IsPrime(numbers[i])) // only return if number is prime
+    {
+        Console.WriteLine($"{numbers[i]}");
+    }
+}
---
+++ Lab0/Utilities.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab0
+{
+    /// <summary>
+    /// Methods for getting and validating input data and relevant calculation
+    /// </summary>
+    internal static class Utilities
+    {
+        /// <summary>
+        /// reads positive decimal
+        /// </summary>
+        /// <param name="prompt"> name used in error messages</param>
+        /// <returns>decimal value read that is > 0</returns>
+        public static decimal GetPositiveDecimal(string prompt)
+        {
+            bool gotIt = false;
+            decimal result = 0;
+            string input;
+
+            while (!gotIt)
+            {
+                Console.WriteLine($"Enter {prompt}:");
+                input = Console.ReadLine() ?? "";
+
+                if (Decimal.TryParse(input, out result))
+                {
+                    if (result > 0)
+                    {
+                        gotIt = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error: {prompt} must be positive");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {prompt} must be a number");
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// reads positive int
+        /// </summary>
+        /// <param name="prompt"> name used in error messages</param>
+        /// <returns>int value read that is > 0</returns>
+        public static int GetPositiveInt(string prompt)
+        {
+            bool gotIt = false;
+            int result = 0;
+            string input;
+
+            while (!gotIt)
+            {
+                Console.WriteLine($"Enter {prompt}:");
+                input = Console.ReadLine() ?? "";
+
+                if (Int32.TryParse(input, out result))
+                {
+                    if (result > 0)
+                    {
+                        gotIt = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error: {prompt} must be positive");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {prompt} must be a number");
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// reads int in range
+        /// </summary>
+        /// <param name="prompt">name used in error messages</param>
+        /// <param name="minValue">minimum value for the range</param>
+        /// <param name="maxValue">maximum value for the range</param>
+        /// <returns>int value read that is within range</returns>
+        public static int GetIntInRange(string prompt, int minValue, int maxValue)
+        {
+            bool gotIt = false;
+            int result = 0;
+            string input;
+
+            while (!gotIt)
+            {
+                Console.WriteLine($"Enter {prompt}:");
+                input = Console.ReadLine() ?? "";
+
+                if (Int32.TryParse(input, out result))
+                {
+                    if (result >= minValue && result <= maxValue)
+                    {
+                        gotIt = true;
+                    }
+                    else
+                    {
+                        if (minValue == Int32.MinValue)
+                            Console.WriteLine($"Error: {prompt} must be less than or equal to {maxValue}");
+                        else if (maxValue == Int32.MaxValue)
+                            Console.WriteLine($"Error: {prompt} must be greater than or equal to {minValue}");
+                        else
+                            Console.WriteLine($"Error: {prompt} must be within range {minValue} - {maxValue}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {prompt} must be a number");
+                }
+            }
+            return result;
+        }
+
+        public static bool IsPrime(int num)
+        {
+            if (num <= 1) return false;
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    } // class
+} // namespace
